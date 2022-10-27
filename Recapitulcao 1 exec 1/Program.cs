82f5bdc4f7f11db0c750d60305828f10f3b066d4
Rafael Marques <rafaelmarques.rm86@gmail.com>
2022-10-27 11:41:12 -0300

    Recapitulação

---
+++ Recapitulcao 1 exec 1/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma 
+mensagem explicativa, conforme exemplos. */
+
+int a, b, soma;
+Console.WriteLine("Informe o primeiro numero:");
+a = int.Parse(Console.ReadLine());
+Console.WriteLine("Informe o segundo numero:");
+b = int.Parse(Console.ReadLine());
+soma = a + b;
+Console.WriteLine("Soma = " + soma);
